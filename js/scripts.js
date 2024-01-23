66f2091a655977b6835d37aa606ca3a352629672
RhysMarch <c1012902@newcastle.ac.uk>
2024-01-23 21:20:39 +0000

    Boids now face where they are moving

--- js/scripts.js
+++ js/scripts.js
@@ -26,12 +26,13 @@ renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
 
 class Boid {
     constructor() {
-        const geometry = new THREE.ConeGeometry(0.1, 0.25, 32);
+        const geometry = new THREE.ConeGeometry(0.1, 0.2, 32);
+        geometry.rotateX(Math.PI / 2);
         const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
         this.mesh = new THREE.Mesh(geometry, material);
-        const speed = 0.05; // Adjust this value as needed for desired speed
-        this.velocity = new THREE.Vector2(Math.random() - 0.5, Math.random() - 0.5).normalize().multiplyScalar(speed);
-        this.acceleration = new THREE.Vector2();
+        const speed = 0.04; // Adjust this value as needed for desired speed
+        this.velocity = new THREE.Vector3(Math.random() - 0.5, Math.random() - 0.5, 0).normalize().multiplyScalar(speed);
+        this.acceleration = new THREE.Vector3();
 
         this.mesh.position.set(
             (Math.random() - 0.5) * frustumSize * aspect,
@@ -72,6 +73,11 @@ class Boid {
 
         // Reset acceleration for the next frame
         this.acceleration.set(0, 0);
+
+        // Add the current position to the direction to get a point in space the boid should look at
+        const direction = new THREE.Vector3(this.velocity.x, this.velocity.y, 0);
+        direction.add(this.mesh.position);
+        this.mesh.lookAt(direction);
     }
 
     // Add methods for behaviors here (Separation, Alignment, Cohesion)
